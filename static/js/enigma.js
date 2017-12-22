1b14532bd5374b56b06c2c946c6ea7cb97a1ec09
dthung1602 <dthung1602@gmail.com>
2017-12-22 17:41:27 +0700

    add random setting, add more css

--- static/js/enigma.js
+++ static/js/enigma.js
@@ -42,3 +42,43 @@ function updateValue(plug) {
             plug.value = ""
     }
 }
+
+
+function getRandomItems(list, num) {
+    var newList = [];
+    while (newList.length < num) {
+        var item = list[Math.floor(Math.random() * list.length)];
+        if (newList.indexOf(item) < 0)
+            newList.push(item)
+    }
+    return newList;
+}
+
+function randomizeSetting() {
+    var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+
+    // reflection wheel
+    getByName("rw").value = getRandomItems(["B", "C"], 1);
+
+    // rotator wheels
+    var wheels = getRandomItems(["I", "II", "III", "IV", "V", "VI", "VII", "VIII"], 4);
+    for (var i = 0; i < 4; i++)
+        getByName("w" + (i + 1)).value = wheels[i];
+
+    // init position
+    var letters = getRandomItems(alphabet, 4);
+    for (i = 0; i < 4; i++)
+        getByName("p" + (i + 1)).value = letters[i];
+
+    // plug board
+    for (i = 0; i < 26; i++)
+        getByName(alphabet[i]).value = "";
+    var n = Math.floor(Math.random() * 14);
+    letters = getRandomItems(alphabet, n * 2);
+    for (i = 0; i < n; i++) {
+        var u = letters[2 * i];
+        var v = letters[2 * i + 1];
+        getByName(u).value = v;
+        getByName(v).value = u;
+    }
+}
